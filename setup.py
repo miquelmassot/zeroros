fee2cd74677578110d592aceda3551bec66ade07
Miquel Massot <miquel.massot@gmail.com>
2024-01-26 07:10:23 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="zeroros",
-    version="1.0.7",
+    version="1.0.8",
     description="ZeroROS middleware",
     long_description=long_description,
     long_description_content_type="text/markdown",
