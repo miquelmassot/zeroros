99fce255f2398a87fbc236e19febc65c9b1ceca9
Miquel Massot <miquel.massot@gmail.com>
2024-01-26 09:29:28 +0000

    Add topic names to logs

--- src/zeroros/datalogger.py
+++ src/zeroros/datalogger.py
@@ -15,11 +15,13 @@ class DataLogger:
         # Open the log file
         self.file = open(self.log_file, "w")
 
-    def log(self, msg: type[Message]):
+    def log(self, msg: type[Message], topic_name: str = "unknown"):
         # Write message to file
         self.file.write(
             '{"class": "'
             + str(type(msg).__name__)
+            + '", "topic_name": "'
+            + topic_name
             + '", "timestamp": "'
             + str(datetime.datetime.utcnow().timestamp())
             + '", "message": '
