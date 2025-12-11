2f3fe9c0e031ee8bc868af55c14fea02c512de18
Blair-insitu <B.Thornton@soton.ac.uk>
2025-12-11 16:04:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="zeroros",
-    version="1.1.0",
+    version="1.1.1",
     description="ZeroROS middleware",
     long_description=long_description,
     long_description_content_type="text/markdown",
